46d56a5558c2c7641a0b4c98d5f1c85ae3329fc3
Paulo <pauloh16.pdr@gmail.com>
2023-06-03 19:43:02 -0300

    feat: criar listagem de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { FlatList } from 'react-native';
 import React from 'react';
 
 import { Highlight, GroupCard } from '@components/index';
@@ -5,11 +6,17 @@ import { Highlight, GroupCard } from '@components/index';
 import * as Styles from './styles';
 
 const Groups: React.FC = () => {
+	const [groups, setGroups] = React.useState<string[]>(['Galera da Rocket']);
+
 	return (
 		<Styles.Container>
 			<Highlight title='Turmas' subtitle='Jogue com a sua turma' />
 
-			<GroupCard title='Primeiro' />
+			<FlatList
+				data={groups}
+				keyExtractor={(key) => key}
+				renderItem={({ item }) => <GroupCard title={item} />}
+			/>
 		</Styles.Container>
 	);
 };
