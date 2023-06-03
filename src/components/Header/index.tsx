191d9ea3b1a3c618cdbca0ef51a45def3bc07531
Paulo <pauloh16.pdr@gmail.com>
2023-06-03 19:02:38 -0300

    feat: ajustar header com botao de voltar e acessibilidade

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,25 @@ import * as Styles from './styles';
 
 import LogoFile from '@images/logo.png';
 
-const Header: IComponent = () => {
+interface IHeaderProps {
+	showBackArrow?: boolean;
+}
+
+const Header: IComponent<IHeaderProps> = ({ showBackArrow, ...props }) => {
 	return (
-		<Styles.Container>
+		<Styles.Container showBackArrow={showBackArrow} {...props}>
+			{showBackArrow && (
+				<Styles.BackArrowButton
+					accessible
+					role='button'
+					accessibilityRole='button'
+					accessibilityHint='Irá voltar para tela anterior.'
+					accessibilityLabel='Botão de voltar a tela.'
+				>
+					<Styles.BackArrow />
+				</Styles.BackArrowButton>
+			)}
+
 			<Styles.Logo
 				role='img'
 				accessibilityRole='image'
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,14 +1,34 @@
 import styled from 'styled-components/native';
+import { CaretLeft } from 'phosphor-react-native';
 
-export const Container = styled.View`
+export interface IContainerProps {
+	showBackArrow?: boolean;
+}
+
+export const Container = styled.View<IContainerProps>`
 	width: 100%;
+	padding: 8px;
 
 	flex-direction: row;
 	align-items: center;
-	justify-content: center;
+
+	justify-content: ${({ showBackArrow }) =>
+		showBackArrow ? 'space-between' : 'center'};
 `;
 
 export const Logo = styled.Image`
 	width: 46px;
 	height: 55px;
 `;
+
+export const BackArrowButton = styled.TouchableOpacity`
+	width: 60px;
+	padding: 4px 2px;
+
+	justify-content: flex-start;
+`;
+
+export const BackArrow = styled(CaretLeft).attrs(({ theme }) => ({
+	color: theme.colors.white,
+	size: 32,
+}))``;
